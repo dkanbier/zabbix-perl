4552cec6e80f4a91ee8e33f428f95901a1e52e32
dkanbier <dkanbier@zabbix-dev.kanbier.lan>
2014-03-04 16:11:10 +0100

    Initial upload

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 zabbix-perl
 ===========
+This is a simple Perl module to wrap API calls to Zabbix. 
 
-Simple Perl module to use with the Zabbix API
+Examples
+--------
+Check if a host exists in Zabbix:
+
+```perl
+#!/usr/bin/perl
+
+use Zabbix;
+
+my $zabbix = Zabbix:new("Zabbix","http://zabbix-dev.kanbier.lan","apiuser","changeme");
+
+my @params = (
+	{
+		"host" => "Zabbix Server"
+	}
+);
+
+my $result = $zabbix->go("host","exists",@params);
+
+if ($result->{'result'}) {
+	print "Host exists in Zabbix\n";
+} else {
+	print "Host doesn't exist in Zabbix\n";
+}```
---
+++ Zabbix.pm
@@ -0,0 +1,91 @@
+package Zabbix;
+################################################################################
+# module: Zabbix.pm - Simple Perl module to interact with the Zabbix API
+################################################################################
+#
+# Original script by: https://github.com/sjohnston
+#
+################################################################################
+
+use strict;
+use JSON::XS;
+use LWP::UserAgent;
+
+sub new {
+    my ($class, $url, $user, $password) = @_;
+
+    my $self = {};
+
+    bless $self, $class;
+		
+    my $ua = LWP::UserAgent->new;
+    $ua->agent("Zabbix");
+
+    my $req = HTTP::Request->new(POST => "$url/api_jsonrpc.php");
+    $req->content_type('application/json-rpc');
+
+    $req->content(encode_json( {
+        jsonrpc => "2.0",
+        method => "user.authenticate",
+        params => {
+            user => $user,
+            password => $password,
+        },
+        id => 1,
+    }));
+
+    my $res = $ua->request($req);
+
+    unless ($res->is_success) {
+      die "Can't connect to Zabbix" . $res->status_line;
+    }
+
+    my $auth = decode_json($res->content)->{'result'};
+
+
+        $self->{'UserAgent'} = $ua;
+        $self->{'Request'} = $req;
+        $self->{'Count'} = 1;
+        $self->{'Auth'} = $auth;
+
+   return $self;
+}
+
+sub ua {
+    return shift->{'UserAgent'};
+}
+
+sub req {
+    return shift->{'Request'};
+}
+
+sub auth {
+    return shift->{'Auth'};
+}
+
+sub next_id {
+    return ++shift->{'Count'};
+}
+
+sub go {
+    my ($self, $object,$action, $params) = @_;
+	
+    my $req = $self->req;
+    $req->content(encode_json( {
+        jsonrpc => "2.0",
+        method => $object.".".$action,
+        params => $params,
+        auth => $self->auth,
+        id => $self->next_id,
+    }));
+
+    my $res = $self->ua->request($req);
+	
+    unless ($res->is_success) {
+      die "Can't connect to Zabbix" . $res->status_line;
+    }
+
+    return decode_json($res->content);
+}
+
+1;
